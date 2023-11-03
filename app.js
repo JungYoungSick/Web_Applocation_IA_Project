0e188e08ae6b8eee4983c7b3aeed62c496f2fa02
JungYoungSick <junfas1216@gmail.com>
2023-11-03 15:59:13 +0900

    server.js 1차 작성, index.html CSS 스타일 이동, Login.html CSS 링크 삭제.

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const http = require("http")
-const fs = require("fs")
+const express = require('express');
+const app = express();
+const port = 8080;
 
+app.use(express.static('static'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/Login.html');
+});
+app.post('/', (req, res) => {
+  const submittedUsername = req.body.userID;
+  const submittedPassword = req.body.userPW;
+  const realUsername = 'green';
+  const realPassword = '1234';
+  if (submittedUsername === realUsername && submittedPassword === realPassword) {
+    res.sendFile(__dirname + '/static/index.html'),
+      res.sendFile(__dirname + '/static/index.css'),
+      res.sendFile(__dirname + '/static/index.js')
+  } else {
+    res.send('로그인 실패')
+  }
+});
+app.listen(port, () => {
+  console.log(`
+서버가 포트 ${port}에서 실행 중입니다.
+http://localhost:${port}
+`);
+});
