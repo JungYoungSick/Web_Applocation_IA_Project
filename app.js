6525e56e8e64ae13a6fd8743e38329be304b8e62
JungYoungSick <junfas1216@gmail.com>
2023-11-03 17:54:53 +0900

    서버 내용 수정, Login.html이 표시되어야 하나 index.html이 표기되는 문제 오류있음

--- app.js
+++ app.js
@@ -10,25 +10,26 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/static/Login.html');
 });
-app.post('/', (req, res) => {
-  const submittedUsername = req.body.userID;
-  const submittedPassword = req.body.userPW;
-  const realUsername = 'green';
-  const realPassword = '1234';
-  if (submittedUsername === realUsername && submittedPassword === realPassword) {
-    app.post('/static/index.html', (req, res) => {
-      res.sendFile(__dirname + '/static/index.html'),
-        app.get('/static/index.css', (req, res) => {
-          res.sendFile(__dirname + '/static/index.css'),
-            app.get('/static/index.js', (req, res) => {
-              res.sendFile(__dirname + '/static/index.js')
-            });
-        });
-    });
-  } else {
-    res.send('로그인 실패')
-  }
-});
+// app.post('/', (req, res) => {
+//   const userID = req.body.userID;
+//   const userPW = req.body.userPW;
+//   const realUserID = 'green';
+//   const realUserPW = '1234';
+//   if (userID === realUserID && userPW === realUserPW) {
+//     app.get('/static/index.html', (req, res) => {
+//       // 현재 작업이 여기로 바로 연결이 되는 문제가 있다.
+//       res.sendFile(__dirname + '/static/index.html'),
+//         app.get('/static/index.css', (req, res) => {
+//           res.sendFile(__dirname + '/static/index.css'),
+//             app.get('/static/index.js', (req, res) => {
+//               res.sendFile(__dirname + '/static/index.js')
+//             });
+//         });
+//     });
+//   } else {
+//     res.send('로그인 실패')
+//   }
+// });
 app.listen(port, () => {
   console.log(`
 서버가 포트 ${port}에서 실행 중입니다.
