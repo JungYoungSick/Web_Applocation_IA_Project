30e4cfab0b12a4b447d11eb8b724ec0bc76a1250
JungYoungSick <junfas1216@gmail.com>
2023-11-04 23:24:55 +0900

    app.use(express.static('public'))부분 public이 아닌 static으로 되어있어 작동이 안했었던 것 수정 완료

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
