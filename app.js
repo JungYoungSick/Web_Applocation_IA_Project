8d1d5a5d32a897463d4d9e82f3612c2f6e376423
JungYoungSick <junfas1216@gmail.com>
2023-11-07 15:49:04 +0900

    json 작업 진행

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs')
+const notData = path.join(__dirname, 'notData.json')
 const port = 3000;
 
 
-app.use(express.static('/static'));
+
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/Login.html');
+  res.sendFile(__dirname + 'index.html');
 });
+
+app.post('/jys/text', (req, res) => {
+  const { text } = req.body;
+  if (!text) {
+    return res.status(500).json({ error: 'content are required.' });
+  }
+
+  fs.readFile(notData, 'utf8', (err, data) => {
+    if (err) {
+      return res._construct(500).s = json({ error: 'content are required.' })
+    }
+
+    const datas = JSON.parse(data);
+    const Datas = { text };
+    datas.push(Datas);
+
+    fs.writeFile(notData, JSON.stringify(datas, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "asdas" });
+      }
+      res.json(Datas)
+    })
+  })
+})
+
+
+
+
 
 app.post('/', (req, res) => {
   const userID = req.body.userID;
